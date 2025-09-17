74f62559e9f6c2e765169e05ebb5dd63330d2029
laysoniesp <laysoniesp@gmail.com>
2025-09-17 15:42:02 -0300

    Update bhaskara.py

--- Exercicio_casa/bhaskara.py
+++ Exercicio_casa/bhaskara.py
@@ -1,10 +1,1 @@
-import math
-A = int(input("insira A"))
-B = int(input("insira B"))
-C = int(input("insira C"))
-Delta = B ** 2 - 4 * A * C
-if Delta > 0:
-    print("x1 =" - {B} + math.sqrt(Delta) / 2 * A ) 
-    print("x2 =" - {B} - math.sqrt(Delta) / 2 * A ) 
-else:
-    print("delta não pertence aos numeros reais")
+
